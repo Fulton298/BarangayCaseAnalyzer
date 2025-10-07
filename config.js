b6e9e9c4a974e77f7c8ba7e9e48550965c9332e0
Fulton298 <144081900+Fulton298@users.noreply.github.com>
2025-10-07 22:08:10 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,11 @@
+window.LEGAL_SEARCH = {
+  GOOGLE_API_KEY: AIzaSyD_KzKLx-SuLAUQ3mi6RLmTF3JMLE-pvzI,
+  GOOGLE_CSE_ID: 95b6ee40941014bcb,
+  ALLOWED_SITES: [
+    "lawphil.net",
+    "chanrobles.com",
+    "sc.judiciary.gov.ph",
+    "ca.judiciary.gov.ph",
+    "officialgazette.gov.ph"
+  ]
+};
